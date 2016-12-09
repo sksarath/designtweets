3ddd924753e6cff243fbcc525054fe5033e701a6
sarath <sarath.r@olacabs.com>
2016-12-09 16:58:15 +0530

    inital commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	// $('.twitter_container').on(
+ //    {
+ //      mouseenter: function() 
+ //    {
+ //        $('.twitter_container').not(this).fadeTo('fast', 0.5);
+ //    },
+ //    mouseleave: function()
+ //    {
+ //        $('.twitter_container').fadeTo('fast', 1);
+ //    }
+ //    });
+});
+
+
+
+
+
+
+
